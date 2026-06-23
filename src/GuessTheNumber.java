3e13198458235a42a9d66880a1b031f5a9122475
techmoocher <92527665+techmoocher@users.noreply.github.com>
2026-06-23 15:00:20 +0700

    feat: make Karu more engaging

--- src/GuessTheNumber.java
+++ src/GuessTheNumber.java
@@ -1,31 +1,32 @@
 import java.util.Scanner;
 
 public class GuessTheNumber {
-    private static final String RESET  = "\u001B[0m";
-    private static final String PINK   = "\u001B[35m";
-    private static final String RED    = "\u001B[31m";
-    private static final String GREEN  = "\u001B[32m";
-    private static final String CYAN   = "\u001B[36m";
-    private static final String YELLOW = "\u001B[33m";
-
-    private static int      target;
-    private static byte     level = 1;
-    private static byte     chances = 10;
+    /* GLOBALS */
+    private static final String RESET   = "\u001B[0m";
+    private static final String PINK    = "\u001B[35m";
+    private static final String RED     = "\u001B[31m";
+    private static final String GREEN   = "\u001B[32m";
+    private static final String CYAN    = "\u001B[36m";
+    private static final String YELLOW  = "\u001B[33m";
 
     private static int[]    bestAttempt = {-1, -1, -1};
-    private static double[] bestTime = {-1.0, -1.0, -1.0};
+    private static double[] bestTime    = {-1.0, -1.0, -1.0};
+
+    private static int      target;
+    private static byte     level       = 1;
+    private static byte     chances     = 10;
+    private static boolean  isFirstPlay = true;
 
     private static final String[] messages = 
     {
-        "Loading your guess... (ᐢ..ᐢ )", "Checking the answer... (ᐢ..ᐢ )",
-        "Verifying your guess... (ᐢ..ᐢ )", "Analyzing your guess... (ᐢ..ᐢ )",
-        "Assessing your guess... (ᐢ..ᐢ )", "Evaluating your guess... (ᐢ..ᐢ )",
-        "Finalizing the result... (ᐢ..ᐢ )", "Calculating the result... (ᐢ..ᐢ )",
-        "Determining the outcome... (ᐢ..ᐢ )", "Processing your request... (ᐢ..ᐢ )"
+        "Pretending to read your guess... (ᐢ..ᐢ )", "Consulting my crystal ball... (ᐢ..ᐢ )",
+        "Verifying if you're a genius... (ᐢ..ᐢ )", "Analyzing your dumbness... (ᐢ..ᐢ )",
+        "Evaluating your life choices... (ᐢ..ᐢ )", "Calculating how wrong you are... (ᐢ..ᐢ )",
+        "Finalizing your doom... (ᐢ..ᐢ )", "Processing your lucky guess... (ᐢ..ᐢ )",
+        "Determining the comedic outcome... (ᐢ..ᐢ )", "Humoring your request... (ᐢ..ᐢ )"
     };
 
-    private static boolean isFirstPlay = true;
-
+    /* HELPERS */
     private static void sleep(int time) {
         try {
             Thread.sleep(time);
@@ -34,13 +35,13 @@ public class GuessTheNumber {
         }
     }
 
-    public static void foxSpeak(String text, int delay, int downtime, boolean lineBreak) {
+    private static void foxSpeak(String text, int delay, int downtime, boolean lineBreak) {
         for (char c : text.toCharArray()) {
             System.out.print(c);
             switch (c) {
-                case ','    -> sleep(delay + 300);
-                case '.'    -> sleep(delay + 500);
-                case ('?')  -> sleep(delay + 500);
+                case ','    -> sleep(delay + 200);
+                case '.'    -> sleep(delay + 400);
+                case '?'    -> sleep(delay + 400);
                 default     -> sleep(delay);
             }
         }
@@ -50,14 +51,15 @@ public class GuessTheNumber {
         sleep(downtime);
     }
 
-    public static void levelSelection(Scanner sc) {
+    private static void levelSelection(Scanner sc) {
         if (!isFirstPlay) {
             sc.nextLine();
 
-            foxSpeak("So now you know the game, do you wish to play again with the same level?\n", 50, 1000, false);
+            foxSpeak("You survive!? Wanna stick to the same level?\n(you don't have have to, you know...", 35, 500, true);
             System.out.print(">>> Enter your choice [Y/n]: ");
 
-            while (true) {
+            boolean ok = false;
+            while (!ok) {
                 String inp = sc.nextLine().trim();
 
                 if (inp.isEmpty() || inp.equalsIgnoreCase("y")) {
@@ -69,11 +71,11 @@ public class GuessTheNumber {
                     return;
                 }
                 else if (inp.equalsIgnoreCase("n")) {
-                    break;
+                    ok = true;
                 }
                 else {
                     System.out.println(RED + "INVALID INPUT!" + RESET +
-                                        "\n\nPlease select a valid option.");
+                                        "\n\nHuh? It's a Y/N question dumbass.");
                     System.out.print(">>> Enter your choice [Y/n]: ");
                 }
             }
@@ -88,52 +90,56 @@ public class GuessTheNumber {
         while (true) {
             while (!sc.hasNextInt()) {
                 System.out.println(RED + "INVALID INPUT!" + RESET +
-                                    "\n\nPlease select a valid option (1-3).");
+                                    "\n\nI said A NUMBER from 1 to 3. A NUMBER!!!");
+                foxSpeak("Did your mom teach you to tell numbers and letters apart??", 35, 300, true);
                 System.out.print(">>> Enter your choice: ");
                 sc.next();
             }
             int inp = sc.nextInt();
             if (inp >= 1 && inp <= 3) {
                 level = (byte) inp;
+                sc.nextLine();
                 break;
             }
             else {
                 System.out.println(RED + "OUT OF BOUNDS!" + RESET +
-                                    "\n\nPlease select a valid option (1-3).");
+                                    "\n\n1, 2, or 3, alright? Counting is hard, I know...");
+                foxSpeak("Counting is not easy, I know...", 35, 300, true);
                 System.out.print(">>> Enter your choice: ");
             }
         }
 
         switch (level) {
             case 1 -> {
-                foxSpeak(PINK + "Scared!? Aren't you ꒰ᐢ. . ᐢ꒱" + RESET, 50, 1000, true);
+                foxSpeak(PINK + "Easy? How adorable... >3<" + RESET, 40, 750, true);
                 chances = 10;
             }
             case 2 -> {
-                foxSpeak(PINK + "Ohhh...Bold, yet not too close ( ˙꒳​˙ )" + RESET, 60, 1000, true);
+                foxSpeak(PINK + "Medium? Boring...but nice try. <3" + RESET, 40, 750, true);
                 chances = 5;
             }
             case 3 -> {
-                foxSpeak(PINK + "W-what!? You can't beat me anyways ( ｡•̀ᴗ-)✧" + RESET, 70, 1000, true);
+                foxSpeak(PINK + "Hard!? Ohhh ~~ Promise me you won't cry >_." + RESET, 40, 750, true);
                 chances = 3;
             }
         }
     }
 
-    public static void game(Scanner sc) {
+    private static void game(Scanner sc) {
+        int guess = -1;
         int attempts = 0;
         long startTime, endTime;
 
         startTime = System.nanoTime();
         while (chances > 0) {
             System.out.print(">>> Enter your guess (" + chances + " left): ");
-            boolean ok = false;
-            int guess = -1;
             
+            boolean ok = false;
             while (!ok) {
                 while (!sc.hasNextInt()) {
                     System.out.println(RED + "INVALID INPUT!!!" + RESET +
-                                        "\n\nEnter a valid non-negative integer (1-100).");
+                                        "\n\nThat's not a number dumbass.");
+                    foxSpeak("Maybe you should check your keyboard layout.", 35, 300, true);
                     System.out.print(">>> Enter your guess: ");
                     sc.next();
                 }
@@ -142,30 +148,48 @@ public class GuessTheNumber {
                     guess = inp;
                     ok = true;
                 }
+                else {
+                    System.out.println(RED + "OUT OF BOUNDS!" + RESET +
+                                        "\n\nThe number must be between 1 and 100, alright?");
+                    foxSpeak("Teacher, could you please teach them to count?", 35, 300, true);
+                    System.out.print(">>> Enter your choice: ");
+                }
             }
             
             attempts++;
             chances--;
 
-            int downtime = (int)(Math.random() * (2000 - 800 + 1)) + 800;   
+            int downtime = (int)(Math.random() * (1500 - 500 + 1)) + 500;
             int rnd = (int)(Math.random() * 10);
             System.out.println(CYAN + messages[rnd] + RESET);
             sleep(downtime);
 
-            if (guess < target) {
-                System.out.println(RED + "INCORRECT!" + RESET +
-                                    " Don't underestimate me ;)\n");
-            }
-            else if (guess > target) {
-                System.out.println(RED + "INCORRECT!" + RESET +
-                                    " You're overcomplicating things >:(\n");
+            if (guess != target) {
+                if (guess < target) {
+                    System.out.println(RED + "INCORRECT!" + RESET +
+                                        " Too low buddy! Don't underestimate me. ;)\n");
+                }
+                else if (guess > target) {
+                    System.out.println(RED + "INCORRECT!" + RESET +
+                                        " Too high blud! Just double-digit numbers, not rocket science. >:(\n");
+                }
+
+                if (chances == 0) {
+                    System.out.println(RED + "GAME OVER!!!" + RESET +
+                                        "\nOhhh ~~ Looks like someone has no chances left.");
+                    sleep(500);
+                    System.out.println("The correct answer was " + YELLOW + target + RESET +
+                                        ". Better luck next time, hoo-man!\n");
+                    break;
+                }
             }
             else {
                 endTime = System.nanoTime();
                 double timeTaken = (endTime - startTime) / 1_000_000_000.0;
                 
                 System.out.println(GREEN + "CORRECT! You nailed it in " + attempts + " attempt(s)." + RESET);
-                System.out.printf(GREEN + "It took you %.2f seconds!!!%n" + RESET, timeTaken);
+                System.out.printf(GREEN + "It took you %.2f seconds!" + RESET +
+                                    "I suspect it's pure luck tho...%n", timeTaken);
 
                 int idx = level - 1;
                 if (bestAttempt[idx] == -1 || attempts < bestAttempt[idx]) {
@@ -175,20 +199,12 @@ public class GuessTheNumber {
                     bestTime[idx] = timeTaken;
                 }
                 
-                foxSpeak(PINK + "Uhhh...That's pretty impressive, but NOT that close!!! ꒰ ˙꒳​˙ ꒱\n" + RESET, 80, 1000, true);
+                foxSpeak(PINK + "Purr...Bet you're just lucky this time. Wanna tray again, hoo-man? :333\n" + RESET, 40, 800, true);
                 break;
             }
         }
 
-        if (isFirstPlay) {
-            isFirstPlay = false;
-        }
-
-        if (chances < 0) {
-            System.out.println(RED + "GAME OVER!!!" + RESET + "\nYou ran out of chances.");
-            sleep(500);
-            System.out.println("The correct answer was " + YELLOW + target + RESET + ".\n");
-        }
+        isFirstPlay = false;
     }
 
     public static void main(String[] args) {
@@ -198,67 +214,86 @@ public class GuessTheNumber {
             do {
                 repeat = false;
 
-                foxSpeak(PINK + "HELLO..." + RESET, 100, 500, true);
-                foxSpeak(PINK + "I'm Karu, a naughty fox /\\. ./\\\n" + RESET, 80, 800, true);
-                foxSpeak("Welcome to ", 50, 500, false);
-                foxSpeak("GUESS THE NUMBER!!! (fox limited edition)\n", 80, 1000, true);
+                if (isFirstPlay) {
+                    foxSpeak(PINK + "HELLO..." + RESET, 100, 500, true);
+                    foxSpeak(PINK + "I'm Karu, a naughty fox" + RESET, 80, 0, true);
+                    System.out.println("/\\../\\\n");
+                    sleep(500);
 
-                foxSpeak("The rules are easyyyy ~~~\n", 80, 500, false);
-                foxSpeak("I'm thinking of a number between 1 and 100, and you have to get it right.\n", 50, 1000, true);
+                    foxSpeak("Welcome to ", 50, 500, false);
+                    foxSpeak("GUESS THE NUMBER!!! (fox limited edition)\n", 50, 1000, true);
+
+                    foxSpeak("The rules are simple ~~~", 40, 300, true);
+                    foxSpeak("I think of a number between 1 and 100, and you have to get it right.\n", 30, 800, true);
+                }
                 
                 target = (int)(Math.random() * 100) + 1;
-
+                
                 levelSelection(sc);
 
-                System.out.println("\nYou're all set. Let's start the game!\n");
+                foxSpeak("\nAlright, you're all set. Don't embarrass yourself ~~~\n", 30, 800, true);
 
                 game(sc);
 
-                while (true) {
+                boolean ok = false;
+                while (!ok) {
                     System.out.println("Anyways, what do you wanna do now?");
-                    System.out.println("1. Play again!!!");
-                    System.out.println("2. View time records.");
-                    System.out.println("3. View attempt count records.");
-                    System.out.println("4. Quit");
+                    System.out.println("1. Play again (!!!)");
+                    System.out.println("2. View your 'speedy' time records");
+                    System.out.println("3. View your attempt count records");
+                    System.out.println("4. Quit (that's alright I understand)");
                     System.out.print(">>> Enter your choice: ");
 
                     byte cmd = -1;
                     while (true) {
                         while (!sc.hasNextInt()) {
                             System.out.println(RED + "INVALID INPUT!" + RESET +
-                                                "\nPlease select a valid option (1-4).");
+                                                "\n\nA NUMBER from 1 to 4, please ~~~.");
                             System.out.print(">>> Enter your choice: ");
                             sc.next();
                         }
                         int inp = sc.nextInt();
                         if (inp >= 1 && inp <= 4) {
                             cmd = (byte) inp;
+                            sc.nextLine();
                             break;
                         }
                         else {
                             System.out.println(RED + "OUT OF BOUNDS!" + RESET +
-                                                "\n\nPlease select a valid option (1-3).");
+                                                "\n\nPick a number from 1 to 4, please ~~~");
+                            foxSpeak("Counting is not that hard, right?", 35, 300, true);
                             System.out.print(">>> Enter your choice: ");
                         }
                     }
 
                     switch (cmd) {
-                        case 1 -> repeat = true;
+                        case 1 -> {
+                            repeat = true;
+                            ok = true;
+                        }
                         case 2 -> {
-                            System.out.println(YELLOW + "\n--- Your Personal Time Records ---" + RESET);
+                            System.out.println(YELLOW + "\n--- Your Time Records ---" + RESET);
                             System.out.printf("1. [EASY]   %s%n", bestTime[0] == -1.0 ? "No record yet" : bestTime[0] + "s");
                             System.out.printf("2. [MEDIUM] %s%n", bestTime[1] == -1.0 ? "No record yet" : bestTime[1] + "s");
                             System.out.printf("3. [HARD]   %s%n\n", bestTime[2] == -1.0 ? "No record yet" : bestTime[2] + "s");
-                            sleep(3500);
+                            System.out.print("\nPress any keys to return to the menu...");
+                            sc.nextLine();
+                            System.out.println();
                         }
                         case 3 -> {
-                            System.out.println(YELLOW + "\n--- Your Personal Attempt Records ---" + RESET);
+                            System.out.println(YELLOW + "\n--- Your Attempt Count Records ---" + RESET);
                             System.out.printf("1. [EASY]   %s%n", bestAttempt[0] == -1 ? "No record yet" : bestAttempt[0] + " attempts");
                             System.out.printf("2. [MEDIUM] %s%n", bestAttempt[1] == -1 ? "No record yet" : bestAttempt[1] + " attempts");
                             System.out.printf("3. [HARD]   %s%n\n", bestAttempt[2] == -1 ? "No record yet" : bestAttempt[2] + " attempts");
-                            sleep(3500);
+                            System.out.print("\nPress any keys to return to the menu...");
+                            sc.nextLine();
+                            System.out.println();
                         }
-                        default -> foxSpeak(PINK + "S-sorry to see you go... (╥﹏╥)" + RESET, 80, 0, false);
+                        case 4 -> {
+                            foxSpeak(PINK + "Awww, why so soon? S-sorry to see you go... (T~T)" + RESET, 80, 0, false);
+                            repeat = false;
+                            ok = true;
+                        }
                     }
                 }
             } while (repeat);
